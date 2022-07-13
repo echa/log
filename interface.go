3b49a3f85f9e8ff97c80e96d2b6c49d3a469a9c9
Alexander Eichhorn <alex@blockwatch.cc>
2022-07-13 20:46:41 +0400

    Add log closure

--- interface.go
+++ interface.go
@@ -84,3 +84,20 @@ func NewLogger(tag string) Logger {
 		return New(NewConfig()).NewLogger(tag)
 	}
 }
+
+// Closure is a closure that can be printed with %v to be used to
+// generate expensive-to-create data for a detailed log level and avoid doing
+// the work if the data isn't printed.
+type Closure func() string
+
+// String invokes the log closure and returns the results string.
+func (c Closure) String() string {
+	return c()
+}
+
+// NewLogClosure returns a new closure over the passed function which allows
+// it to be used as a parameter in a logging function that is only invoked when
+// the logging level is such that the message will actually be logged.
+func NewClosure(c func() string) Closure {
+	return Closure(c)
+}
