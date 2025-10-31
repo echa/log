b59b613289bc61d0084bab6110674dc0f46c4073
Abdul-Harisu Inusah <28383750+ankorGH@users.noreply.github.com>
2025-10-31 10:00:27 +0000

    update export functions

--- registry.go
+++ registry.go
@@ -10,7 +10,13 @@ import (
 
 const wildcard = "*"
 
-var r = newRegistry()
+var (
+	r            = newRegistry()
+	NewLogger    = r.New
+	GetLogger    = r.Get
+	RemoveLogger = r.Remove
+	SetLevels    = r.SetLevels
+)
 
 type registry struct {
 	mu  sync.RWMutex
@@ -35,24 +41,20 @@ func (r *registry) New(tag string) (logger Logger) {
 	}
 }
 
-func NewLogger(tag string) Logger {
-	return r.New(tag)
-}
-
-func RemoveLogger(tag string) {
+func (r *registry) Remove(tag string) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	delete(r.reg, tag)
 }
 
-func GetLogger(tag string) (Logger, bool) {
+func (r *registry) Get(tag string) (Logger, bool) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	logger, ok := r.reg[tag]
 	return logger, ok
 }
 
-func SetLevels(tag string, lvl Level) {
+func (r *registry) SetLevels(tag string, lvl Level) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
